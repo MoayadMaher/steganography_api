f5212e922c6ebedf5422688cd9b1aa7516a756d5
MoayadMaher <its.moayadj@gmail.com>
2024-05-12 16:37:19 +0300

    small fix

--- services.py
+++ services.py
@@ -1,19 +1,18 @@
 import subprocess
+import logging
 from utils import *
 
 async def hide_text(image_url: str, text: str) -> dict:
     try :
         image_path = download_image(image_url)
         text_file_path = write_text_to_file(text)
-
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        stego_image_path = f"stego_image_{timestamp}.png"
+        stego_image_path = f"stego_image_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
 
         command = [
-                "python", "secret_pixel.py", "hide",
-                image_path, text_file_path, "public.pub", stego_image_path
-            ]
-        subprocess.run(command, check=True)
+            "python3", "secret_pixel.py", "hide",
+            image_path, text_file_path, "public.pub", stego_image_path
+        ]
+        subprocess.run(command, check=True )
         
         cloudinary_url = upload_to_cloudinary(stego_image_path)
         cleanup_files([image_path, text_file_path, stego_image_path])
